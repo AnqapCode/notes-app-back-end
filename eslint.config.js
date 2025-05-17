006979121d81a32fc60d98382df0c93635955a10
AnqapCode <arifcahyogd7@gmail.com>
2025-05-17 19:25:14 +0700

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import daStyle from "eslint-config-dicodingacademy";
+
+export default [
+  daStyle,
+  // other config style
+  {
+    rules: {
+      "no-console": "off",
+      quotes: "off",
+      "linebreak-style": "off",
+    },
+  },
+];
